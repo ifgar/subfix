539e809118e8183bdf53525f42ac25ee930edda4
ignaciofgar <ignaciofgar@proton.me>
2025-11-29 15:17:32 +0100

    Added AppColors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFFC0CAF5);
+  static const Color secondary = Color(0xFF565f89);
+  static const Color tertiary = Color(0xFF292E42);
+  
+  static const Color accentBlue = Color(0xFF7DCFFF);
+  static const Color accentRed = Color(0xFFC53B53);
+  static const Color background = Color(0xFF1F2335);
+}
