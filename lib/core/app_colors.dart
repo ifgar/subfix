3ed5d686101feebcc0f1d68e6291f4245c302c9c
ignaciofgar <ignaciofgar@proton.me>
2025-11-29 15:27:03 +0100

    Added TextStyles and some new colors

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,11 +1,16 @@
 import 'dart:ui';
 
 class AppColors {
+  // Basic colors
   static const Color primary = Color(0xFFC0CAF5);
-  static const Color secondary = Color(0xFF565f89);
-  static const Color tertiary = Color(0xFF292E42);
-  
+  static const Color secondary = Color(0xFF8289B0);
+  static const Color tertiary = Color(0xFF565F89);
+
+  // Accents
   static const Color accentBlue = Color(0xFF7DCFFF);
   static const Color accentRed = Color(0xFFC53B53);
-  static const Color background = Color(0xFF1F2335);
+
+  // Backgrounds
+  static const Color backgroundDark = Color(0xFF1F2335);
+  static const Color backgroundLight = Color(0xFF292E42);
 }
---
+++ lib/core/text_styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:subfix/core/app_colors.dart';
+
+class TextStyles {
+  static const TextStyle bodyText = TextStyle(
+    color: AppColors.primary,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle bodyTitle = TextStyle(
+    color: AppColors.primary,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle bodyComment = TextStyle(
+    color: AppColors.secondary,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+}
