e56e7240445908a5431b09a13a87c7ded904209b
ignaciofgar <ignaciofgar@proton.me>
2025-11-29 20:04:06 +0100

    added window size properties, min size and default launch size

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:subfix/core/app_colors.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:subfix/home_screen.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if(Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
+    
+    // Window min size
+    setWindowMinSize(const Size(500, 700));
+    setWindowMaxSize(Size.infinite);
+
+    // Window launch size
+    setWindowFrame(const Rect.fromLTWH(100, 100, 500, 700));
+  }
   runApp(const MainApp());
 }
 
