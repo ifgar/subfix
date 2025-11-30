5631f9d9abe7a56aba020a83bb10da6f62997cbb
ignaciofgar <ignaciofgar@proton.me>
2025-11-30 22:26:59 +0100

    Finished formatTime function

--- lib/core/sync_subs.dart
+++ lib/core/sync_subs.dart
@@ -12,6 +12,54 @@ Offset processOffset(double offset) {
 }
 
 String formatTime(int h, int m, int s, int ms) {
+  // ms
+  if (ms < 0) {
+    int borrow = ((ms.abs() + 999) / 1000).toInt();
+    ms += borrow * 1000;
+    s -= borrow;
+  }
 
-  return "";
+  if (ms >= 1000) {
+    int carry = (ms / 1000).toInt();
+    ms %= 1000;
+    s += carry;
+  }
+
+  // s
+  if (s < 0) {
+    int borrow = ((s.abs() + 59) / 60).toInt();
+    s += borrow * 60;
+    m -= borrow;
+  }
+
+  if (s >= 60) {
+    int carry = (s / 60).toInt();
+    s %= 60;
+    m += carry;
+  }
+
+  // m
+  if (m < 0) {
+    int borrow = ((m.abs() + 59) / 60).toInt();
+    m += borrow * 60;
+    h -= borrow;
+  }
+
+  if (m >= 60) {
+    int carry = (m / 60).toInt();
+    m %= 60;
+    h += carry;
+  }
+
+  // h
+  if (h < 0) {
+    h = 0;
+    m = 0;
+    s = 0;
+    ms = 0;
+  }
+  return "${h.toString().padLeft(2,'0')}:"
+       "${m.toString().padLeft(2,'0')}:"
+       "${s.toString().padLeft(2,'0')},"
+       "${ms.toString().padLeft(3,'0')}";
 }
