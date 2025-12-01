3ac441e92bc9664205a68f1fe7b117e8264ff139
ignaciofgar <ignaciofgar@proton.me>
2025-12-01 21:50:08 +0100

    Improved custom MenuBar with hover color and leading icon

--- lib/components/custom_menu_bar.dart
+++ lib/components/custom_menu_bar.dart
@@ -11,45 +11,93 @@ class CustomMenuBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MenuBarWidget(
-      barStyle: MenuStyle(backgroundColor: WidgetStatePropertyAll(AppColors.backgroundLight), shape: WidgetStatePropertyAll(RoundedRectangleBorder())),
+      barButtonStyle: ButtonStyle(
+        minimumSize: WidgetStatePropertyAll(const Size(0, 32)),
+        backgroundBuilder: (context, states, child) {
+          final hovered = states.contains(WidgetState.hovered);
+
+          return Stack(
+            alignment: Alignment.centerLeft,
+            children: [
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    height: hovered ? 26 : 0,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: hovered ? AppColors.tertiary : Colors.transparent,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                ),
+              ),
+
+              child!,
+            ],
+          );
+        },
+        fixedSize: WidgetStatePropertyAll(const Size.fromHeight(36)),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        ),
+        overlayColor: WidgetStatePropertyAll(AppColors.backgroundLight),
+      ),
+      barStyle: MenuStyle(
+        backgroundColor: WidgetStatePropertyAll(AppColors.backgroundLight),
+        shape: WidgetStatePropertyAll(RoundedRectangleBorder()),
+      ),
       barButtons: [
         BarButton(
-            text: const Text('File', style: TextStyles.bodyText),
-            submenu: SubMenu(
-                menuItems: [
-                    MenuButton(
-                        text: const Text('Save'),
-                        onTap: () {},
-                        icon: const Icon(Icons.save),
-                        shortcutText: 'Ctrl+S',
-                    ),
-                    const MenuDivider(),
-                    MenuButton(
-                        text: const Text('Exit'),
-                        onTap: () {},
-                        icon: const Icon(Icons.exit_to_app),
-                        shortcutText: 'Ctrl+Q',
-                    ),
-                ],
-            ),
+          text: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [Icon(Icons.abc, color: AppColors.primary)],
+          ),
+          submenu: SubMenu(menuItems: []),
         ),
         BarButton(
-            text: const Text('Help', style: TextStyles.bodyText),
-            submenu: SubMenu(
-                menuItems: [
-                    MenuButton(
-                      text: const Text('View License'),
-                      onTap: () {},
-                    ),
-                    MenuButton(
-                      text: const Text('About'),
-                      onTap: () {},
-                      icon: const Icon(Icons.info),
-                    ),
-                ],
-            ),
+          text: const Text(
+            'File',
+            style: TextStyles.bodyText,
+            textAlign: TextAlign.center,
+          ),
+          submenu: SubMenu(
+            menuItems: [
+              MenuButton(
+                text: const Text('Save'),
+                onTap: () {},
+                icon: const Icon(Icons.save),
+                shortcutText: 'Ctrl+S',
+              ),
+              const MenuDivider(),
+              MenuButton(
+                text: const Text('Exit'),
+                onTap: () {},
+                icon: const Icon(Icons.exit_to_app),
+                shortcutText: 'Ctrl+Q',
+              ),
+            ],
+          ),
         ),
-    ],
+        BarButton(
+          text: const Text(
+            'Help',
+            style: TextStyles.bodyText,
+            textAlign: TextAlign.center,
+          ),
+          submenu: SubMenu(
+            menuItems: [
+              MenuButton(text: const Text('View License'), onTap: () {}),
+              MenuButton(
+                text: const Text('About'),
+                onTap: () {},
+                icon: const Icon(Icons.info),
+              ),
+            ],
+          ),
+        ),
+      ],
       child: child,
     );
   }
