226a6b00ea9de152bad0ff6d59af4c54734d9403
ignaciofgar <ignaciofgar@proton.me>
2025-12-20 15:32:44 +0100

    improve CustomButton customizability

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -1,44 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:subfix/core/app_theme.dart';
+import 'package:subfix/core/color_utils.dart';
 import 'package:subfix/core/text_styles.dart';
 
 class CustomButton extends StatelessWidget {
   final String title;
   final VoidCallback onPressed;
   final AppTheme activeTheme;
+  final bool filled;
+  final int width;
 
   const CustomButton({
     super.key,
     required this.title,
     required this.onPressed,
     required this.activeTheme,
+    required this.filled,
+    this.width = 96,
   });
 
   @override
   Widget build(BuildContext context) {
-    Color accentDarker = HSLColor.fromColor(activeTheme.accent)
-        .withLightness(
-          (HSLColor.fromColor(activeTheme.accent).lightness - 0.1).clamp(
-            0.0,
-            1.0,
-          ),
-        )
-        .toColor();
     return Material(
-      color: activeTheme.accent,
+      color: filled ? activeTheme.accent : activeTheme.backgroundPrimary,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadiusGeometry.circular(8),
-        side: BorderSide(color: accentDarker),
+        side: BorderSide(
+          color: filled ? Colors.transparent : activeTheme.accent,
+        ),
       ),
       child: InkWell(
         onTap: () => onPressed,
         borderRadius: BorderRadius.circular(8),
-        splashColor: accentDarker,
+        splashColor: filled
+            ? darken(activeTheme.accent, -0.05)
+            : darken(activeTheme.backgroundPrimary, -0.025),
+        hoverColor: filled
+            ? darken(activeTheme.accent, 0.05)
+            : darken(activeTheme.backgroundPrimary, 0.025),
         child: SizedBox(
           height: 32,
-          width: 96,
+          width: width.toDouble(),
           child: Center(
-            child: Text(title, style: TextStyles.buttonText(activeTheme)),
+            child: Text(
+              title,
+              style: filled
+                  ? TextStyles.buttonText(activeTheme)
+                  : TextStyles.altButtonText(activeTheme),
+            ),
           ),
         ),
       ),
