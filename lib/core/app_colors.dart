76b856c02ea9320b9ab8ca9aaa3c737f5ad88b00
ignaciofgar <ignaciofgar@proton.me>
2025-12-28 22:19:32 +0100

    set preliminary AppColors

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -6,7 +6,6 @@ class AppColors {
   // Basic colors
   static late Color primary;
   static late Color secondary;
-  static late Color tertiary;
 
   // Accents
   static late Color accent;
@@ -19,10 +18,9 @@ class AppColors {
   static void init(GtkThemeData themeData) {
     primary = Color(themeData.theme_text_color);
     secondary = Color(themeData.theme_text_color);
-    tertiary = Color(themeData.theme_text_color);
     accent = Color(themeData.theme_selected_bg_color);
     accentRed = Color(themeData.error_color);
     backgroundDark = Color(themeData.theme_bg_color);
-    backgroundLight = Color(themeData.theme_bg_color);
+    backgroundLight = Color(themeData.theme_base_color);
   }
 }
